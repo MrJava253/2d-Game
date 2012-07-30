867b2ff7a5526d56b322c5f4635b8fd77f57e637
Aleksander <aleksander@ubuntu.ubuntu-domain>
2012-07-30 23:26:15 +0200

    Some code

--- src/net/alek345/twodgame/Display.java
+++ src/net/alek345/twodgame/Display.java
@@ -1,12 +1,20 @@
 package net.alek345.twodgame;
 
-public class Display{
+import java.awt.Canvas;
+
+import javax.swing.JFrame;
+
+public class Display extends Canvas{
+	private static final long serialVersionUID = 1L;
 	
 	static String TITLE = "2d Game";
+	
+	private final static int WIDTH = 800;
+	private final static int HEIGHT = 600;
 
 	public static void main(String[] args){
 		JFrame f = new JFrame(TITLE);
-		f.setSize(800, 600);
+		f.setSize(WIDTH, HEIGHT);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setVisible(true);
 	}
